042202cd33f8d8b161979792e7c4b1801c1c73d9
kldobrev <kldobrev@gmail.com>
2017-02-19 22:29:22 +0200

    Defined the logic of Race.score

--- lib/race.rb
+++ lib/race.rb
@@ -3,7 +3,16 @@ module RaceBet
 
     class << self
       def score(guesses, winners)
-
+        points = 0
+        score_rules = [15, 10, 5, 3, 1]
+        guesses.each_with_index do |guess, ind|
+          if guess == winners[ind] 
+            points += score_rules[ind]
+          elsif winners.include? guess
+            points += 1
+          end
+        end
+        return points
       end
     end
 
